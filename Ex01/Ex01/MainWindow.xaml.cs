cd44775ff25dac03081481e489e9913b417c8479
Camily Kirlly Pereira Meireles <kirlly.p@academico.ifrn.edu.br>
2019-10-09 15:13:01 -0300

    salvar arquivo

---
+++ Ex01/Ex01/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex01
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string esc;
+            Console.WriteLine("Esolha se q para quadrado e r para retângulo");
+            esc = Console.ReadLine();
+            if (esc == "q")
+            {
+
+                Console.WriteLine("Digite o valor do lado");
+                Quadrado q = new Quadrado(double.Parse(Console.ReadLine()));
+                Console.WriteLine(q);
+
+            }
+            else if (esc == "r")
+            {
+                int b, h;
+                Console.WriteLine("Digite a base");
+                b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a altura");
+                h = int.Parse(Console.ReadLine());
+                Retangulo r = new Retangulo(b, h);
+                Console.WriteLine(r);
+            }
+
+
+        }
+    }
+}
---
+++ Ex01/Ex01/Retangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class Retangulo
+    {
+        private double b;
+        private double h;
+        public Retangulo(double b, double h)
+        {
+            this.b = b;
+            this.h = h;
+
+        } 
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+        public double CalcArea()
+        {
+            return b * h;
+        }
+        public double CalcDiagonal()
+        {
+            
+            return Math.Sqrt(Math.Pow(b,2) + Math.Pow(h,2));
+        }
+        public override string ToString()
+        {
+            return $"{b} - {h} - {GetBase()} - {GetAltura()}"; 
+        }
+
+    }
+}
